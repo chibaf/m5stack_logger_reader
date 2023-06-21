989a1abe7ff1c3df3f4535b83d8ce6625af1dd2d
chibaf <chibaf@mac.com>
2023-06-21 23:29:43 +0900

    Add files via upload

--- read_m5_class.py
+++ read_m5_class.py
@@ -9,9 +9,13 @@ class m5logger:
       return [0.0]*10
     data = [str(val) for val in line2.split(",")]
     data1=[]
-    for i in range(0,10):
-      try:
-        data1.append(float(data[i+2]))
-      except IndexError:
-        return [0.0]*10
+    if len(data)==12:
+      for i in range(0,10):
+        try:
+          fd=float(data[i+2])
+        except:
+          return [0.0]*10
+        data1.append(fd)
+    else:
+      return [0.0]*10
     return data1
